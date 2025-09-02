27a59ad189d03431e3698abb5b7baef3513388ba
selimreza777 <selimrezacse@gmail.com>
2025-09-02 15:25:25 +0600

    repo create

---
+++ let-const.js
@@ -0,0 +1,30 @@
+// es6
+// const er man change kora jai na ja thake oitai dete hoy
+
+// **** when you declear any valu const and let then you always declear korbo before initailization 
+// console.log(name);
+const name = 'bangladesh';
+const countryName = 'Bd' + name;
+// console.log(countryName);
+
+let perice = 120;
+price = 150;
+// console.log(price);
+
+// const charge = 50;
+// charge = 45;
+
+const dress = ['shirt', 'pant', 'shoe'];
+
+dress.push('maflar');
+console.log(dress);
+
+
+const student = { name: 'hemel-parul', marks: 50 }
+student.marks = 60;
+console.log(student)
+
+
+
+
+
