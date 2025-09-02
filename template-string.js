4f027d1163db785fa28c1cc9e932b9c50e6e450b
selimreza777 <selimrezacse@gmail.com>
2025-09-02 17:28:24 +0600

    template-string.js

---
+++ template-string.js
@@ -0,0 +1,27 @@
+// const poem = 'amm pata jora jora \n' +
+//   'marbo chabuk chourbo ghora \n' + 'ore khoka ghure dara'
+
+// ```` [back tree bole esc er nice keybord e ace]
+//bact tree ` ` use jar jonne line auto brek kore nai
+const myPoem = `amm pata jora jora
+marbo chabuk ghure dara
+asche amar pagla ghora
+ghora dekhe palash na
+dood makha vatt khabi na
+rater beal asbi na`
+
+// console.log(myPoem);
+
+function addition(sum1, sum2) {
+  const relult = sum1 + sum2;
+  // const output = 'sum of ' +  sum1 + ' add ' + sum2 + ' is equal to ' + relult;
+  // console.log(output)
+  // `` use next line how to use and wright 
+  const output = `sum of number ${sum1 * 5} and ${sum2}` // ${} ei vabe likle javascrip er ekta validation hoy otherwise onno vabe eta hoy na
+  // console.log(output)
+}
+addition(50, 70)
+
+const price = 50;
+const discountPrice = `discount price of ${price} after discount 20% ${price * 0.8}`
+console.log(discountPrice)
