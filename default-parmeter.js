73b9959f1458809810273e22c09f7ca3c3eb2a93
selimreza777 <selimrezacse@gmail.com>
2025-09-02 15:56:04 +0600

    default undefinde NaN

--- default-parmeter.js
+++ default-parmeter.js
@@ -1,5 +1,29 @@
-function sum(num1, num2) {
-  const total = num1 + num2;
-  console.log(total);
+// function sum(num1, num2) { //num1 , num2 = bole prameter
+//   const total = num1 + num2;
+//   console.log(num1, num2, total);
+// }
+// sum(23, 24, 56, 45, 23,35); // 23, 34 = bole arguments
+
+function multiply(num1 = 1, num2 = 1) {
+  const result = num1 * num2;
+  console.log(result);
 }
-sum(23, 24);
+multiply(10);
+
+function fullName(fast, second = '1') {
+  const name = fast + '' + second;
+  console.log(name);
+}
+fullName("kamruzzamn")
+
+/*
+* add , substarct  ---> 0
+*
+* multiply ---> 1
+*
+* string ----> ''
+*
+* array =[]
+*
+* object = {}
+*/ 
