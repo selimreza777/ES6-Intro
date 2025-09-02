f732f2956be13896f78484afc94714aca054bbf2
selimreza777 <selimrezacse@gmail.com>
2025-09-03 02:52:57 +0600

    entries

---
+++ entries.js
@@ -0,0 +1,17 @@
+const employeeData = {
+  name: 'papiya',
+  id: 234,
+  salarry: 345456,
+  gender: 'women',
+  experience: 6
+}
+
+
+// objcet.values and objcet.keys 
+const keys = Object.keys(employeeData);
+const values = Object.values(employeeData)
+const entries = Object.entries(employeeData)
+console.log(keys)
+console.log(values)
+// 2 dimentinal kore property and vlue dicce 
+console.log(entries);
